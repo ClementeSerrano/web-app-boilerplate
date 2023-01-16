bd41db8e9b5057c1707a06ed6df7a400b66b2797
ClementeSerrano <clementeserranosutil@gmail.com>
2023-01-16 19:40:35 +0100

    :sparkles: Make AuthButton component to open AccountsManager modal
    Action being trigger when avatar link is clicked

--- src/app/modules/Accounts/components/AuthButton/AuthButton.tsx
+++ src/app/modules/Accounts/components/AuthButton/AuthButton.tsx
@@ -1,45 +1,43 @@
-import { Link, useLocation } from 'react-router-dom';
-import { useTheme } from 'styled-components';
+import { useLocation } from 'react-router-dom';
 
 import useCurrentAccount from '../../../Accounts/hooks/useCurrentAccount';
-import { useAccounts } from '../../context/AccountsContext.hooks';
 import Avatar from '../../../../components/Avatar/Avatar';
 import { ROUTE_PATHS } from '../../../../routes/routes.constants';
 import { WithStyle } from '../../../../components/components.types';
-// import AccountsManager from '../AccountsManager/AccountsManager';
+import Navlink from '../../../../components/Navlink/Navlink';
 
 export default function AuthButton({ style, className }: WithStyle) {
   const location = useLocation();
 
   const [currentAccount] = useCurrentAccount();
 
-  const accounts = useAccounts();
-
-  const theme = useTheme();
-
-  console.log({ accounts });
-
   if (currentAccount) {
-    // TODO: Make avatar component that opens accounts manager in modal.
     return (
-      <Avatar
-        alt={currentAccount.metadata.name || 'User avatar'}
-        withInitials
-        style={{ marginRight: theme.spacing.xs }}
-      />
+      <Navlink
+        to={ROUTE_PATHS.accountsManager}
+        container
+        size="sm"
+        state={{ background: location }}
+        style={{ padding: 2, borderRadius: '50%', ...style }}
+        className={className}
+      >
+        <Avatar
+          alt={currentAccount.metadata.name || 'User avatar'}
+          withInitials
+        />
+      </Navlink>
     );
   }
 
   return (
-    <>
-      <Link
-        to={ROUTE_PATHS.addAccount}
-        state={{ background: location }}
-        style={style}
-        className={className}
-      >
-        Log in
-      </Link>
-    </>
+    <Navlink
+      to={ROUTE_PATHS.addAccount}
+      container
+      state={{ background: location }}
+      style={style}
+      className={className}
+    >
+      Log in
+    </Navlink>
   );
 }
