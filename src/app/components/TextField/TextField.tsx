886149f5e4496278490b89a0eb985726ad9ebc2f
ClementeSerrano <clementeserranosutil@gmail.com>
2023-01-21 13:47:42 +0100

    :memo: Add docs for TextField

--- src/app/components/TextField/TextField.tsx
+++ src/app/components/TextField/TextField.tsx
@@ -10,6 +10,40 @@ import {
   TextFieldLabel,
 } from './TextField.styles';
 
+/**
+ * Allow users to enter text into a UI. Typically appear in
+ * forms and dialogs.
+ * @param props - All props that a HTML input element receives (InputHTMLAttributes).
+ * Extra props that receive:
+ * @param props.adornments - Prefix or suffix components to add at the start or end of
+ * the main component. "left" and "right" inputs are optional.
+ * @param props.error - Boolean flag that allows to add error styles to the input if an
+ * error from outside controller happens (optional).
+ * @param props.label - Label to add to the input (optional).
+ * @param props.style - Additional styles to add to all internal input components (container,
+ * input, inputContainer, bar, label and highlight).
+ * @param props.variant - Variant of the input to use ("standard" | "filled").
+ * Default is "standard".
+ * @example
+ * A full example of usage is:
+ * ```
+ * function Component(){
+ *   const [value, setValue] = useState("");
+ *
+ *   return (
+ *     <TextField
+ *       id="my-input"
+ *       name="my-input"
+ *       value={value}
+ *       onChange={event => setValue(event.target.value)}
+ *       variant="filled"
+ *       label="My input"
+ *       placeholder="My input placeholder"
+ *     />
+ *   )
+ * }
+ * ```
+ */
 export default function TextField({
   id,
   label,
