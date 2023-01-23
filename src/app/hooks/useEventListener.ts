59110165ebd3970bb298af8ca7b7b3d3cbd6f1ed
ClementeSerrano <clementeserranosutil@gmail.com>
2023-01-23 18:15:13 +0100

    :sparkles: Add useEventListener hook

---
+++ src/app/hooks/useEventListener.ts
@@ -0,0 +1,44 @@
+import { RefObject, useEffect, useRef } from 'react';
+
+/**
+ * It takes as parameters a eventName, a call-back functions (handler)
+ * and optionally a reference element to track events.
+ */
+export default function useEventListener<
+  T extends HTMLElement = HTMLDivElement,
+>(
+  eventName: keyof WindowEventMap,
+  handler: (event: Event) => void,
+  element?: RefObject<T>,
+) {
+  // Create a ref that stores handler
+  const savedHandler = useRef<(event: Event) => void>();
+
+  useEffect(() => {
+    // Define the listening target
+    const targetElement: T | Window = element?.current || window;
+    if (!(targetElement && targetElement.addEventListener)) {
+      return;
+    }
+
+    // Update saved handler if necessary
+    if (savedHandler.current !== handler) {
+      savedHandler.current = handler;
+    }
+
+    // Create event listener that calls handler function stored in ref
+    const eventListener = (event: Event) => {
+      // eslint-disable-next-line no-extra-boolean-cast
+      if (!!savedHandler?.current) {
+        savedHandler.current(event);
+      }
+    };
+
+    targetElement.addEventListener(eventName, eventListener);
+
+    // Remove event listener on cleanup
+    return () => {
+      targetElement.removeEventListener(eventName, eventListener);
+    };
+  }, [eventName, element, handler]);
+}
