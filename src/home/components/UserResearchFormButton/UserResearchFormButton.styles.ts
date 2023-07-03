4dd5f5d2bbed9aeb497c6efd8ad474e82b594b8e
ClementeSerrano <clementeserranosutil@gmail.com>
2023-07-03 16:39:27 +0200

    :lipstick: Fix user research suvery button colors

--- src/home/components/UserResearchFormButton/UserResearchFormButton.styles.ts
+++ src/home/components/UserResearchFormButton/UserResearchFormButton.styles.ts
@@ -4,11 +4,10 @@ import Button from '../../../common/components/Button/Button';
 export const UserResearchFormButtonContainer = styled(Button)`
   display: flex;
   align-items: center;
-  padding: 8px 16px;
-  border-radius: 24px;
-  background: rgba(255, 255, 255, 0.1);
+  border-radius: ${({ theme }) => theme.shape.borderRadius.md}px;
+  background: ${({ theme }) => theme.palette.background.level2};
   border-width: 1px;
   border-style: solid;
-  border-color: rgba(255, 255, 255, 0.05);
+  border-color: ${({ theme }) => theme.palette.background.level3};
   font-size: 13px;
 `;
